7217fce9e340a3e18c9fc4c5e3146e34eb6f2d1f
Allan <allanbrados@gmail.com>
2019-01-25 17:58:34 -0200

    feat: creates word-chain

--- src/markov_elear/generator.clj
+++ src/markov_elear/generator.clj
@@ -1,1 +1,17 @@
 (ns markov-elear.generator)
+
+(def example "And the Golden Grouse And the Pobble who")
+
+(def words (clojure.string/split example #" "))
+
+(def word-transitions (partition-all 3 1 words))
+
+(defn word-chain [word-transitions]
+  (reduce
+   (fn [r t] (merge-with clojure.set/union r
+                         (let [[a b c] t]
+                           {[a b] (if c #{c} #{})})))
+   {}
+   word-transitions
+   )
+  )
