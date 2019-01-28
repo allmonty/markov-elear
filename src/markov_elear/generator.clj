8ea5d8451071d26be4fb715bb5b7f88c4b865ded
Allan <allanbrados@gmail.com>
2019-01-28 20:04:13 -0200

    feat: walk-chain function

--- src/markov_elear/generator.clj
+++ src/markov_elear/generator.clj
@@ -12,11 +12,26 @@
                          (let [[a b c] t]
                            {[a b] (if c #{c} #{})})))
    {}
-   word-transitions
-   )
-  )
+   word-transitions))
 
 (defn text->word-chain [s]
   (let [words (clojure.string/split s #"[\s|\n]")
         word-transitions (partition-all 3 1 words)]
     (word-chain word-transitions)))
+
+(defn chain->text [chain]
+  (apply str (interpose " " chain)))
+
+(defn walk-chain [prefix chain result]
+  (let [suffixes (get chain prefix)]
+    (if (empty? suffixes)
+      result
+      (let [suffix (first (shuffle suffixes))
+            new-prefix [(last prefix) suffix]
+            result-with-spaces (chain->text result)
+            result-char-count (count result-with-spaces)
+            suffix-char-count (inc (count suffix))
+            new-result-char-count (+ result-char-count suffix-char-count)]
+        (if (>= new-result-char-count 140)
+          result
+          (recur new-prefix chain (conj result suffix)))))))
